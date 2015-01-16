a203c299fb838f62ed015af38a13a8ed2333e5ea
Łukasz Korecki <lukasz@korecki.me>
2015-01-16 16:26:22 +0000

    More arg parsing

--- lib/versionista.rb
+++ lib/versionista.rb
@@ -8,6 +8,14 @@ module Versionista
     options = {}
 
     optparse = OptionParser.new do |opts|
+      opts.banner = <<-EOF
+      Versionista!
+
+
+      Manage version files. To get started run
+
+        versionista --init --version-file ./path/to/File
+      EOF
       opts.on('--version-file FILE', 'Path to version file') do |file|
         options[:file] = File.expand_path(file)
       end
@@ -19,15 +27,27 @@ module Versionista
       opts.on('--init', 'Initialize version file') do
         options[:init] = true
       end
+
+      opts.on('-h', 'halp') do
+        puts opts
+        exit 0
+      end
     end
 
     optparse.parse! args
+
+    if options.empty?
+      puts optparse
+      exit 0
+    end
 
     Manager.new(options[:init], options[:file], options[:which])
   end
 
   class Manager < Struct.new(:init, :file, :which)
     def run
+      abort 'No file' if !file || (!File.exist?(file)  && !init)
+      abort "Wrong specifier: #{which}" unless  (%w(minor major patch).include? which) || init
       if init
         log.info "Initializing #{file} with version: 1.0.0"
         File.write(file, '1.0.0')
@@ -37,7 +57,7 @@ module Versionista
       bump!(which)
     end
 
-    def bump! which
+    def bump!(which)
       current_version = File.read(file).strip.chomp
 
       version_hash = Hash[%w(major minor patch).zip(current_version.split('.').map(&:to_i))]
@@ -48,7 +68,7 @@ module Versionista
 
       log.info "Moving from #{current_version} to #{next_version} (in #{file})"
       File.write(file, next_version)
-      log.info "Done"
+      log.info 'Done'
     end
 
     def log
